f842b5f9de1089044ed94eb236b50105ad1b0ba5
Warren Nelson <nelsonwarren155@gmail.com>
2025-06-21 19:34:24 +0200

    Fix second Vercel error

--- src/app/api/auth/callback/layout.tsx
+++ src/app/api/auth/callback/layout.tsx
@@ -1,7 +1,7 @@
 // app/(auth)/sign-in/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+import "@/app/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
